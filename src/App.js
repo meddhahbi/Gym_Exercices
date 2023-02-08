0fc381d51de594668c4a56d89c8d54cd89253e02
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-02-09 00:00:34 +0100

    make a navbar design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
+import ExerciceDetails from './Pages/ExerciceDetails';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Footer from './Components/Footer';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box width="400px" sx={{width:{ x1 : '1488px'}}} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercice/:id" element={<ExerciceDetails />} />
+        </Routes>
+        <Footer />
+
+    </Box>
+  )
 }
 
-export default App;
+export default App
